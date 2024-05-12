3785a22d8928f983597d34fb141c4354c9b3f504
amadeusantos <amadeusantos31@gmail.com>
2024-05-12 16:09:28 -0300

    [feat] config vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    // 👋 add the line below to add jsdom to vite
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './tests/setup.js',
+  }
 })
