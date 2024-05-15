977ba17b6bb62ead0fa26f11bd564933846693f3
Jamuelton <jamueltonangelim@gmail.com>
2024-05-15 10:52:14 -0300

    chore: criando parte inicial da home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>home</div>;
+import * as S from "./styles";
+
+export function Home() {
+  return (
+    <S.Container>
+      <div>navbar</div>
+      <S.Content>
+        <div>
+          <h2>Nome da Empresa</h2>
+          <S.Line style={{ borderTop: "1px solid #244bc5" }} />
+        </div>
+        <div>dashboard</div>
+      </S.Content>
+    </S.Container>
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../pages/home";
+
 import { Login } from "../pages/login";
 import { SignUp } from "../pages/signup";
+import { Home } from "../pages/home";
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
