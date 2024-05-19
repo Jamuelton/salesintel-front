edef28214bb5f7c6cf3d815268ffd669ae450ffe
Jamuelton <jamueltonangelim@gmail.com>
2024-05-18 21:22:11 -0300

    adicionando aprte de produtos

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,70 @@
+import {
+  ArrowCircleRight,
+  Basket,
+  DotsThreeOutline,
+  PlusSquare,
+  Receipt,
+} from "@phosphor-icons/react";
+import { Button } from "../../components/Button";
 import * as S from "./styles";
+import { ProductList } from "../../components/productList";
 
 export function Home() {
+  const PRODUCT_DATA_TEST = [
+    {
+      name: "transmissao",
+      price: "2000,00",
+      quantity: "17",
+    },
+    {
+      name: "motor",
+      price: "5000,00",
+      quantity: "05",
+    },
+    {
+      name: "freio",
+      price: "300,00",
+      quantity: "50",
+    },
+    {
+      name: "bateria",
+      price: "600,00",
+      quantity: "20",
+    },
+    {
+      name: "embreagem",
+      price: "700,00",
+      quantity: "15",
+    },
+    {
+      name: "radiador",
+      price: "400,00",
+      quantity: "10",
+    },
+    {
+      name: "amortecedor",
+      price: "250,00",
+      quantity: "30",
+    },
+    {
+      name: "filtro de ar",
+      price: "50,00",
+      quantity: "100",
+    },
+    {
+      name: "velas de ignição",
+      price: "25,00",
+      quantity: "200",
+    },
+    {
+      name: "pneus",
+      price: "400,00",
+      quantity: "40",
+    },
+  ];
+
+  const itemsToShow = PRODUCT_DATA_TEST.slice(0, 7);
+
   return (
     <S.Container>
       <div>navbar</div>
@@ -9,7 +73,61 @@ export function Home() {
           <h2>Nome da Empresa</h2>
           <S.Line style={{ borderTop: "1px solid #244bc5" }} />
         </div>
-        <div>dashboard</div>
+        <S.PrincipalContent>
+          <S.ProductArea>
+            <S.ProductAreaTitle>
+              <h3>Produtos</h3>
+              <ArrowCircleRight size={32} weight="fill" color="#244bc5" />
+            </S.ProductAreaTitle>
+            <S.ProductAreaList>
+              {itemsToShow.map((item, index) => (
+                <ProductList
+                  key={index}
+                  name={item.name}
+                  price={item.price}
+                  quantity={item.quantity}
+                />
+              ))}
+              {PRODUCT_DATA_TEST.length > 7 && (
+                <S.ProductDots>
+                  <DotsThreeOutline size={32} weight="fill" />
+                </S.ProductDots>
+              )}
+            </S.ProductAreaList>
+          </S.ProductArea>
+          <S.StatusArea>
+            <S.DataArea>
+              <S.AreaOne>area 1</S.AreaOne>
+              <S.RegisterSellerArea>
+                <Button
+                  label="Registrar vendas"
+                  color="#f5f6fa"
+                  secondColor="#244bc5"
+                  icon={<PlusSquare size={20} />}
+                  shape="round"
+                  size="large"
+                />
+                <S.AreaTwo>Area 2</S.AreaTwo>
+              </S.RegisterSellerArea>
+            </S.DataArea>
+            <S.ButtonArea>
+              <Button
+                label="Relatorio de vendas"
+                color="#f5f6fa"
+                secondColor="#244bc5"
+                shape="round"
+                icon={<Basket size={20} />}
+              />
+              <Button
+                label="Relatorio Financeiro"
+                color="#f5f6fa"
+                secondColor="#244bc5"
+                shape="round"
+                icon={<Receipt size={20} />}
+              />
+            </S.ButtonArea>
+          </S.StatusArea>
+        </S.PrincipalContent>
       </S.Content>
     </S.Container>
   );
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -22,3 +22,104 @@ export const Line = styled(Divider)`
     color: var(--blue-primary) !important;
   }
 `;
+
+export const PrincipalContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 90%;
+  gap: 2rem;
+  padding: 1rem;
+`;
+
+export const ProductArea = styled.div`
+  background: var(--white-secondary);
+  padding: 1rem;
+  border-radius: 1rem;
+  width: 50%;
+  height: 100%;
+  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+
+  display: flex;
+  flex-direction: column;
+
+  gap: 12px;
+`;
+
+export const ProductAreaTitle = styled.div`
+  h3 {
+    font-size: 1.5rem;
+    color: var(--blue-primary);
+  }
+
+  display: flex;
+  justify-content: space-between;
+
+  svg {
+    cursor: pointer;
+  }
+`;
+
+export const ProductAreaList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+export const ProductDots = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const StatusArea = styled.div`
+  width: 50%;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 2rem;
+`;
+
+export const AreaOne = styled.div`
+  background: var(--white-secondary);
+  height: 100%;
+  width: 60%;
+  border-radius: 1rem;
+  padding: 1rem;
+  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+`;
+
+export const AreaTwo = styled.div`
+  background: var(--white-secondary);
+  height: 100%;
+  border-radius: 1rem;
+  padding: 1rem;
+  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+`;
+
+export const RegisterSellerArea = styled.div`
+  width: 50%;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 2rem;
+`;
+
+export const DataArea = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 80%;
+  gap: 2rem;
+`;
+
+export const ButtonArea = styled.div`
+  height: 20%;
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  padding: 1rem;
+  background: var(--white-secondary);
+  border-radius: 1rem;
+  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+`;
