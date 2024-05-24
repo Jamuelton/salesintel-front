264dcbe41c8dec83c8f119ece2a135171fa65994
Lucas Monteiro <lucasmonteiromelo@hotmail.com>
2024-05-24 16:06:47 -0300

    feat: adicionando validação do modal de adicionar itens

--- src/components/AddItemsModal/index.tsx
+++ src/components/AddItemsModal/index.tsx
@@ -2,6 +2,8 @@ import * as S from "./styles";
 import { Modal } from "../../components/Modal";
 import { Input } from "../Input";
 import { Button } from "../Button";
+import { useState } from "react";
+import { AddItemsSchema } from "../../services/types/addItemsType";
 
 interface AddItemsModalInterface {
   open?: boolean;
@@ -10,12 +12,54 @@ interface AddItemsModalInterface {
   productName?: string;
 }
 
+interface ErrorInterface {
+  errorType: "" | "warning" | "error" | undefined;
+  errorShow: boolean;
+}
+
 export const AddItemsModal: React.FC<AddItemsModalInterface> = ({
   open,
   onCancel,
   id,
   productName,
 }) => {
+  const [quantity, setQuantity] = useState<number>();
+
+  const [errorquantity, setErrorQuantity] = useState<ErrorInterface>();
+
+  const handleChangeQuantity = (e: { target: { value: string } }) => {
+    const { value } = e.target;
+    try {
+      AddItemsSchema.shape.quantity.parse(Number(value));
+      setErrorQuantity({ errorType: "", errorShow: false });
+    } catch (error) {
+      setErrorQuantity({ errorType: "error", errorShow: true });
+    }
+    setQuantity(Number(value));
+  };
+
+  const submit = () => {
+    if (quantity == undefined) {
+      setErrorQuantity({ errorType: "error", errorShow: true });
+    }
+
+    if (errorquantity?.errorShow != true && quantity != 0) {
+      const json = {
+        quantity: quantity,
+      };
+
+      console.log(json);
+
+      if (onCancel != undefined) {
+        onCancel();
+      }
+
+      console.log("ADICIONADO COM SUCESSO");
+    } else {
+      console.log("Tá com erro");
+    }
+  };
+
   return (
     <Modal
       open={open}
@@ -27,7 +71,7 @@ export const AddItemsModal: React.FC<AddItemsModalInterface> = ({
           shape="round"
           color="#f5f6fa"
           secondColor="#244bc5"
-          buttonFunction={() => console.log("Adicionado")}
+          buttonFunction={submit}
         />,
       ]}
     >
@@ -43,7 +87,16 @@ export const AddItemsModal: React.FC<AddItemsModalInterface> = ({
         </S.InputContainer>
         <S.InputContainer>
           <span>QUANTIDADE:</span>
-          <Input placeholder="QUANTIDADE" type="number" color="#244bc5" />
+          <Input
+            placeholder="QUANTIDADE"
+            type="number"
+            color="#244bc5"
+            min={1}
+            step={1}
+            status={errorquantity?.errorType}
+            errorText={"Insira um valor maior que 0"}
+            inputFunction={handleChangeQuantity}
+          />
         </S.InputContainer>
       </S.ModalForm>
     </Modal>
